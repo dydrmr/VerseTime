aa44982495dc7071b526c1cc14571e5837dba943
dydrmr <mark.a.hofmann@gmail.com>
2022-05-27 08:21:25 -0400

    Added polar-to-cartesian and random functions

--- HelperFunctions.js
+++ HelperFunctions.js
@@ -52,3 +52,38 @@ export function JULIAN_DATE() {
 	let julian = now - date2020;
 	return julian / 1000 / 60 / 60 / 24;
 }
+
+export function POLAR_TO_CARTESIAN(horizontalAngle, verticalAngle, radius) {
+	let radH = RADIANS(horizontalAngle);
+	let radV = RADIANS(verticalAngle);
+
+	let x = Math.cos(radH) * Math.sin(radV) * radius;
+	let y = Math.cos(radV) * radius;
+	let z = Math.sin(radH) * Math.sin(radV) * radius;
+
+	return {x: x, y: y, z: z};
+}
+
+export function RANDOM(min, max) {
+	let rand = Math.random();
+
+	if (typeof min === 'undefined') {
+		return rand;
+
+	} else if (typeof max === 'undefined') {
+		if (min instanceof Array) {
+			return min[Math.floor(rand * min.length)];
+		} else {
+			return rand * min;
+		}
+
+	} else {
+		if (min > max) {
+			const tmp = min;
+			min = max;
+			max = tmp;
+		}
+	}
+
+	return rand * (max - min) + min;
+}
