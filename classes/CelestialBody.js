0ad1119005a11602b77c76ae029bf74bc9751edc
dydrmr <mark.a.hofmann@gmail.com>
2023-11-26 15:38:35 -0500

    Various refactors
    - Changed LENGTH_OF_DAY from function to property
    - Removed unnecessary square rooting and then squaring inside BS function
    - Converted let to const where appropriate

--- classes/CelestialBody.js
+++ classes/CelestialBody.js
@@ -11,6 +11,7 @@ export default class CelestialBody {
 		this.ROTATION_QUATERNION = rotationQuaternion;
 		this.BODY_RADIUS = bodyRadius;
 		this.ROTATION_RATE = rotationRate;
+		this.LENGTH_OF_DAY = 3600 * this.ROTATION_RATE / 86400;
 		this.ANGULAR_ROTATION_RATE = 6 / this.ROTATION_RATE;
 		this.ROTATION_CORRECTION = rotationCorrection;
 
@@ -19,7 +20,7 @@ export default class CelestialBody {
 		// this.ORBITAL_ANGLE = this.PARENT ? Math.atan( (coordinates.y - this.PARENT.COORDINATES.y) / (coordinates.x - this.PARENT.COORDINATES.x) ) : 0;
 
 		if (parseFloat(orbitalRadius) === 0) {
-			this.ORBITAL_RADIUS = (this.PARENT && this.PARENT.COORDINATES) ? this.CALCULATE_ORBITAL_RADIUS() : 0;
+			this.ORBITAL_RADIUS = (this.PARENT && this.PARENT.COORDINATES) ? this.#calculateOrbitalRadius() : 0;
 		} else {
 			this.ORBITAL_RADIUS = orbitalRadius;
 		}
@@ -38,22 +39,22 @@ export default class CelestialBody {
 			direction !== 'y' &&
 			direction !== 'z'
 		) {
-			throw 'Wrong direction value passed to function: ' + direction;
+			throw 'Wrong axis value passed to function: ' + direction;
 			return null;
 		}
 
-		let qw = this.ROTATION_QUATERNION.w;
-		let qx = this.ROTATION_QUATERNION.x;
-		let qy = this.ROTATION_QUATERNION.y;
-		let qz = this.ROTATION_QUATERNION.z;
+		const qw = this.ROTATION_QUATERNION.w;
+		const qx = this.ROTATION_QUATERNION.x;
+		const qy = this.ROTATION_QUATERNION.y;
+		const qz = this.ROTATION_QUATERNION.z;
 
-		let sx = distantObject.COORDINATES.x;
-		let sy = distantObject.COORDINATES.y;
-		let sz = distantObject.COORDINATES.z;
+		const sx = distantObject.COORDINATES.x;
+		const sy = distantObject.COORDINATES.y;
+		const sz = distantObject.COORDINATES.z;
 
-		let bx = this.COORDINATES.x;
-		let by = this.COORDINATES.y;
-		let bz = this.COORDINATES.z;
+		const bx = this.COORDINATES.x;
+		const by = this.COORDINATES.y;
+		const bz = this.COORDINATES.z;
 
 		let part1, part2, part3;
 
@@ -73,67 +74,68 @@ export default class CelestialBody {
             part3 = ((1 - (2 * square(qx)) - (2 * square(qy))) * (sz - bz));
 		}
 
-		let result = part1 + part2 + part3;
-		return result;
+		return part1 + part2 + part3;
 	}
 
 	BS(distantObject) {
-		// Convert from heliocentric coordinates to local coordinates centered on celestial object
-		// "distantObject" is the celestial object that sits at the 0,0,0 origin point (usually the system's star)
-		let x = this.BS_INTERNAL('x', distantObject);
-		let y = this.BS_INTERNAL('y', distantObject);
-		let z = this.BS_INTERNAL('z', distantObject);
+		// Recenter coorinate system on the current celestial body
+		// distantObject is the origin point before the transformation
+		const x = this.BS_INTERNAL('x', distantObject);
+		const y = this.BS_INTERNAL('y', distantObject);
+		const z = this.BS_INTERNAL('z', distantObject);
 
 		return {'x': x, 'y': y, 'z': z};
 	}
 	
 
 	DECLINATION(distantObject = this.PARENT_STAR) {
-		let bs = this.BS(distantObject);
+		const bs = this.BS(distantObject);
 
-		let chunk1 = Math.sqrt( square(bs.x) + square(bs.y) + square(bs.z) );
-		chunk1 = Math.pow(chunk1, 2);
+		const squareX = square(bs.x);
+		const squareY = square(bs.y);
+		const squareZ = square(bs.z);
 
-		let chunk2 = Math.sqrt( square(bs.x) + square(bs.y) );
-		chunk2 = Math.pow(chunk2, 2);
+		const sumXYZ = squareX + squareY + squareZ;
+		const sumXY = squareX + squareY;
 
-		let part1 = chunk1 + chunk2 - square(bs.z);
+		//let chunk1 = Math.sqrt( sumXYZ );
+		//chunk1 = Math.pow(chunk1, 2);
+		//let chunk2 = Math.sqrt( square(bs.x) + square(bs.y) );
+		//chunk2 = Math.pow(chunk2, 2);
+		//const part1 = chunk1 + chunk2 - squareZ;
 
-		let chunk3 = Math.sqrt( square(bs.x) + square(bs.y) + square(bs.z) );
-		let chunk4 = Math.sqrt( square(bs.x) + square(bs.y) );
+		const part1 = sumXYZ + sumXY - squareZ;
 
-		let part2 = 2 * chunk3 * chunk4;
+		const chunk3 = Math.sqrt( sumXYZ );
+		const chunk4 = Math.sqrt( sumXY );
+		const part2 = 2 * chunk3 * chunk4;
 
-		let result = Math.acos(part1 / part2);
+		const result = Math.acos(part1 / part2);
 		return Math.abs(degrees(result));
 	}
 
 	APPARENT_RADIUS(distantObject) {
-		let radius = distantObject.BODY_RADIUS;
-		let bs = this.BS(distantObject);
+		const bs = this.BS(distantObject);
 
-		let p4 = square(bs.x) + square(bs.y) + square(bs.z);
-		let p3 = Math.sqrt(p4);
-		let p2 = radius / p3;
-		let p1 = Math.asin(p2);
+		const p4 = square(bs.x) + square(bs.y) + square(bs.z);
+		const p3 = Math.sqrt(p4);
+		const p2 = distantObject.BODY_RADIUS / p3;
+		const p1 = Math.asin(p2);
 		return degrees(p1);
-	}
-
-	LENGTH_OF_DAY() {
-		return 3600 * this.ROTATION_RATE / 86400;
 	}
 
 	CURRENT_CYCLE() {
 		// How many times the object has rotated since the 2020 reference date
-		let julian = getJulianDate();
-		let dayLength = this.LENGTH_OF_DAY();
-		return julian / dayLength;
+		if (this.LENGTH_OF_DAY === 0) {
+			return 0;
+		} else {
+			return getJulianDate() / this.LENGTH_OF_DAY;
+		}
 	}
 
 	HOUR_ANGLE() {
 		// Current Rotation
-		let cycle = this.CURRENT_CYCLE();
-		if (cycle === Infinity) cycle = 0;
+		const cycle = this.CURRENT_CYCLE();
 
 		const correction = this.ROTATION_CORRECTION;
 		const result = modulo((360 - modulo(cycle, 1) * 360 - correction), 360)
@@ -142,10 +144,10 @@ export default class CelestialBody {
 
 	MERIDIAN(distantObject = this.PARENT_STAR) {
 		// Position of distantObject if this celestial object didn't rotate
-		let bs = this.BS(distantObject);
+		const bs = this.BS(distantObject);
 
-		let p2 = Math.atan2(bs.y, bs.x) - (Math.PI / 2);
-		let p1 = modulo(p2, 2 * Math.PI);
+		const p2 = Math.atan2(bs.y, bs.x) - (Math.PI / 2);
+		const p1 = modulo(p2, 2 * Math.PI);
 		return degrees(p1);
 	}
 
@@ -169,7 +171,7 @@ export default class CelestialBody {
 		}
 	}
 
-	CALCULATE_ORBITAL_RADIUS() {
+	#calculateOrbitalRadius() {
 		return calculateDistance2D(this.COORDINATES.x, this.COORDINATES.y, this.PARENT.COORDINATES.x, this.PARENT.COORDINATES.y);
 	}
 }
